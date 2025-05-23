2bd759e9cb1cd8d977083f1877cead7b2bcca613
Your Name <your.email@example.com>
2025-05-23 10:12:51 +0800

    Site updated: YYYY-05-2025年5月23日 10:12:51

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -1,9 +1,9 @@
 let themeFunc = async function() {
     let btn = document.getElementById("theme-btn");
     let root = document.documentElement;
-    // 设置初始状态为白天模式
-    root.setAttribute('theme', 'light');
-    localStorage.setItem('theme', 'light');
+    // 设置初始状态为黑夜模式
+    root.setAttribute('theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     btn.addEventListener('click', e => {
         if (root.getAttribute('theme') == 'dark') {
             root.setAttribute('theme', 'light');
